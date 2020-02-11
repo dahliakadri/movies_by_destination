635fb866970f499d125c240251a427304e3aecad
dahliakadri <dahliakadri@gmail.com>
2020-02-11 06:47:21 +0000

    update react props

--- static/moodyreact.jsx
+++ static/moodyreact.jsx
@@ -28,11 +28,51 @@ function Header() {
 
 function MainContent() {
 	return(
-		<div className="todo-list">
-			<ToDoItem />
-			<ToDoItem />
-			<ToDoItem />
-		
+		<div className="main">
+			<div className="Joke">
+				<Joke
+					joke={{question: "Question: Knock Knock?", punchline: "who is there?"}} 
+				/>
+				<Joke 
+					joke={{question: "Question: Hahaha?", punchline: "Hehehe"}} 
+				/>
+				<Joke
+					joke={{punchline: "No questions here hehe"}} 
+				 />
+				<Joke 
+					joke={{question: "Question: Ugh Another Q", punchline: "No more plz"}} 
+				/>
+				<Joke 
+					joke={{punchline: "Another punchline only"}} 
+				/>
+			</div>
+			<div className="contacts">
+				<ContactCard 
+                	contact={{name: "Mr. Whiskerson", imgUrl: "http://placekitten.com/300/200",
+                	phone: "(212) 555-1234",email:"mr.whiskaz@catnap.meow"}}
+            	/>
+            
+           		<ContactCard 
+                	contact={{name:"Fluffykins", imgUrl:"http://placekitten.com/400/200",
+                	phone:"(212) 555-2345", email:"fluff@me.com"}}
+            	/>
+            
+            	<ContactCard 
+                	contact={{name:"Destroyer", imgUrl:"http://placekitten.com/400/300",
+                	phone:"(212) 555-3456", email:"ofworlds@yahoo.com"}}
+            	/>
+            
+            	<ContactCard 
+                	contact={{name:"Felix", imgUrl:"http://placekitten.com/200/100",
+                	phone:"(212) 555-1234", email:"mr.whiskaz@catnap.meow"}}
+            	/>
+			</div>
+			
+			<div className="todo-list">
+				<ToDoItem />
+				<ToDoItem />
+				<ToDoItem />
+			</div>
 		</div>
 	)
 }
@@ -51,6 +91,31 @@ function ToDoItem() {
 			<div className="todo-item"><input type="checkbox" />
 			<p>Place 1</p></div>
 	)
+}
+
+function ContactCard(props) {
+	return(
+	<div className="contacts">
+                <img src={props.contact.imgUrl}/>
+                <h3>{props.contact.name}</h3>
+                <p>Phone: {props.contact.phone}</p>
+                <p>Email: {props.contact.email}</p>
+            </div>)
+}
+
+function Joke(props) {
+	if (props.joke.question) {
+		return(
+			<div className="joke">
+				<p>{props.joke.question}</p>
+				<p>Joke:{props.joke.punchline}</p>
+			</div>)
+		} else {
+			return(
+			<div className="joke">
+			<p>Joke:{props.joke.punchline}</p>
+			</div>)
+		}
 }
 
 ReactDOM.render(<MyNewApp />, document.getElementById("root"))
