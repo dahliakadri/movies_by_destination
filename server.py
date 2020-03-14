61c325a4a35b3d96af1080fad804a9bc2596b557
dahliakadri <dahliakadri@gmail.com>
2020-03-14 22:28:21 +0000

    update server and ngnix

--- server.py
+++ server.py
@@ -393,7 +393,7 @@ def view_user_watch_list(user_id):
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
     # point that we invoke the DebugToolbarExtension
-    app.debug = True
+    app.debug = False
     # make sure templates, etc. are not cached in debug mode
     app.jinja_env.auto_reload = app.debug
 
